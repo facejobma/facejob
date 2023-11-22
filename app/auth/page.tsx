8b7b99c4a3dbaa4e6a2140ffdcc54542c105b072
Anas Zenagui <zenaguianas20@gmail.com>
2023-11-22 01:29:14 +0100

    Config the colors theme in tailwindcss + making a structure for a project

---
+++ app/auth/page.tsx
@@ -0,0 +1,5 @@
+export default function Auth(){
+  return (
+    <h1 className="text-primary bg-optional1">Hello to Auth Page</h1>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
+      colors: {
+        primary: "#60894B",
+        secondary: "#424242",
+        third: "#909090",
+        optional1: "#F1F6f9",
+        optional2: "#E3EAF6",
+        optional3: "#F9F9F9",
+      },
+      backgroundImage: {  
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',

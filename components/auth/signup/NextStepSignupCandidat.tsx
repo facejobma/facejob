2047a714c9725d0a8df6706d0328d97e68ecddf1
Anas Zenagui <zenaguianas20@gmail.com>
2023-12-09 15:26:24 +0100

    Complete the auth for Candidat

--- components/auth/signup/NextStepSignupCandidat.tsx
+++ components/auth/signup/NextStepSignupCandidat.tsx
@@ -1,30 +1,176 @@
-// NextStepCandidat.tsx
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+import { useRouter } from 'next/router';
 
-const NextStepSignupCandidat = ({ handleNextStep, onSkip }: any) => {
-  const [additionalInfo, setAdditionalInfo] = useState('');
+interface SecteurOptions {
+  id: number;
+  name: string;
+}
+
+interface NextStepSignupCandidatProps {
+  onSkip: () => void;
+}
+
+const NextStepSignupCandidat: React.FC<NextStepSignupCandidatProps> = ({
+  onSkip,
+}) => {
+  const [bio, setBio] = useState("");
+  const [secteur, setSecteur] = useState("");
+  const [annéeExperience, setAnnéeExperience] = useState("");
+  const [image, setImage] = useState<File | null>(null);
+  const [secteurOptions, setSecteurOptions] = useState<SecteurOptions[]>([]);
+  const maxLength = 250;
+
+  const router = useRouter();
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: (acceptedFiles: File[]) => {
+      setImage(acceptedFiles[0]);
+    },
+  });
+
+  const handleBioChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputValue = e.target.value;
+    if (inputValue.length <= maxLength) {
+      setBio(inputValue);
+    }
+  };
+
+  const remainingCharacters = maxLength - bio.length;
+
+  useEffect(() => {
+    const fetchSectors = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/sectors");
+        const data = await response.json();
+        setSecteurOptions(data);
+      } catch (error) {
+        console.error("Error fetching sectors:", error);
+      }
+    };
+
+    fetchSectors();
+  }, []);
+
+  const handleSubmit = async () => {
+    try {
+      const formData = {
+        user_id: Number(sessionStorage.getItem("userId")) || "",
+        bio,
+        secteur,
+        image,
+        annee_experience: Number(annéeExperience),
+      };
+      // formData.append("user_id", sessionStorage.getItem("userId") || "");
+      // formData.append("bio", bio);
+      // formData.append("secteur", secteur);
+      // formData.append("annee_experience", annéeExperience);
+
+      console.log("formData, ", formData);
+
+      // if (image) {
+      //   formData.append("image", image);
+      // }
+
+      const response = await fetch(
+        "http://localhost:8000/api/complete-candidat",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.ok) {
+        const responseData = await response.json();
+        // console.log(responseData);
+      } else {
+      }
+    } catch (error) {
+      console.error("Error updating user:", error);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center font-default rounded-lg border border-newColor p-4">
       <h2 className="text-3xl font-semibold text-second my-2 py-4 mb-4">
         Additional Information (Optional)
       </h2>
-      <textarea
-        placeholder="Tell us more about yourself..."
-        value={additionalInfo}
-        onChange={(e) => setAdditionalInfo(e.target.value)}
-        className="px-4 py-2 rounded border border-gray w-96 h-32"
-      />
-      <div className="flex space-x-2 my-4">
-        <button
-          onClick={() => handleNextStep(additionalInfo)}
-          className="py-2 px-20 rounded-full font-medium text-base text-white bg-primary"
+
+      <div className="w-96 mb-4">
+        <textarea
+          placeholder="Tell us more about yourself..."
+          value={bio}
+          onChange={handleBioChange}
+          maxLength={maxLength}
+          className="px-4 py-2 rounded border border-gray w-full h-32 text-secondary resize-none"
+        />
+        <p className="text-gray-500 text-sm mt-1 text-end">
+          {remainingCharacters} character{remainingCharacters !== 1 ? "s" : ""}{" "}
+          remaining
+        </p>
+      </div>
+
+      <div className="w-96 mb-4">
+        <select
+          value={secteur}
+          onChange={(e) => setSecteur(e.target.value)}
+          className="px-4 py-2 text-secondary rounded border border-gray w-full appearance-none bg-white focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
         >
-          Next
+          <option value="" disabled>
+            Select Secteur
+          </option>
+          {secteurOptions.map((option, index) => (
+            <option key={index} value={option.name}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div
+        {...getRootProps()}
+        className="w-96 mb-4 border border-gray p-4 rounded"
+      >
+        <input {...getInputProps()} />
+        <p className="text-secondary">
+          Drag 'n' drop an image here, or click to select an image
+        </p>
+        {image && (
+          <div className="mt-5 flex justify-center items-center flex-col ">
+            <p className="text-primary mb-1">Selected Image:</p>
+            <img
+              src={URL.createObjectURL(image)}
+              alt="Selected Image"
+              className="rounded-lg max-w-full h-auto"
+            />
+          </div>
+        )}
+      </div>
+
+      <input
+        type="number"
+        placeholder="Année Experience"
+        value={annéeExperience}
+        onChange={(e) => setAnnéeExperience(e.target.value)}
+        className="px-4 py-2 rounded border border-gray w-96 mb-4 text-secondary"
+      />
+
+      <div className="flex space-x-2">
+        <button
+          onClick={handleSubmit}
+          className="py-2 px-10 rounded-full font-medium text-base text-white bg-primary"
+        >
+          Submit
         </button>
         <button
-          onClick={() => onSkip()}
-          className="py-2 px-20 rounded-full font-medium text-base text-white bg-gray-400"
+          onClick={() => {
+            onSkip();
+            router.push("/auth/login-candidat");
+          }}
+          className="py-2 px-10 rounded-full font-medium text-base text-white bg-gray-400"
         >
           Skip
         </button>

1bffb5783f0e678f43781287f1b1ee5b7cbd7ff8
Anas Zenagui <zenaguianas20@gmail.com>
2023-12-23 12:33:44 +0100

    Refactoring the ForgetPassword + Adding a ResetPasswordSent component

---
+++ components/auth/forgetPassword/index.tsx
@@ -0,0 +1,94 @@
+import { FormEvent, useState } from "react";
+import { toast } from "react-hot-toast";
+import emailIcon from "../../../public/images/envelope.png";
+import Image from "next/image";
+import Link from "next/link";
+import NavBar from "../../../components/NavBar";
+
+type ForgetPasswordFormProps = {
+  onSubmit: (formData: any) => void;
+};
+
+const ForgetPasswordForm = ({ onSubmit }: ForgetPasswordFormProps) => {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!email) {
+      return toast.error("Veuillez remplir le champ email!");
+    }
+
+    const formData = {
+      email,
+    };
+
+    onSubmit(formData);
+  };
+
+  const validateEmail = (value: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(value)) {
+      setEmailError("Adresse e-mail invalide");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  // onSubmit(formData);
+  //   };
+
+  return (
+    <>
+      {/* <NavBar /> */}
+      <div className="flex flex-col mb-6 items-center rounded-lg border border-newColor p-4 font-default max-w-lg mx-auto md:max-w-2xl">
+        <Image
+          src={emailIcon}
+          alt="google"
+          width={90}
+          height={90}
+          className="mt-10 flex space-x-4"
+          // style={{ width: "110px", height: "110px" }}
+        />
+        <h2 className="text-2xl md:text-3xl font-medium text-second my-2 py-4 mb-4">
+          Mettez à jour votre mot de passe
+        </h2>
+        <form className="flex flex-col space-y-6 my-4" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              validateEmail(e.target.value);
+            }}
+            style={{ width: "400px", margin: "auto" }}
+            className={`px-4 py-2 rounded border border-gray ${
+              emailError ? "border-red-500" : ""
+            }`}
+          />
+          {emailError && (
+            <p className="text-red-500 m-12 text-sm">{emailError}</p>
+          )}
+
+          <button
+            type="submit"
+            className="py-2 px-8 m-12 rounded-full font-medium text-base text-white bg-primary"
+          >
+            Envoyer un E-mail
+          </button>
+          <p className="font-normal my-2 mx-auto md:mx-32 text-second">
+            Vous avez déjà un compte ?{" "}
+            <Link href="/auth/login-candidate" className="text-primary">
+              Se connecter
+            </Link>
+          </p>
+          {/* </div> */}
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ForgetPasswordForm;
--- pages/auth/forget-password.tsx
+++ pages/auth/forget-password.tsx
@@ -1,28 +1,23 @@
-import { FormEvent, useState } from "react";
-import { toast } from "react-hot-toast";
-import emailIcon from "../../public/images/envelope.png";
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+import { useState } from "react";
+import ForgetPasswordForm from "../../components/auth/forgetPassword";
+import ResetPasswordSent from "../../components/auth/forgetPassword/ResetPasswordSent";
+import toast from "react-hot-toast";
 import NavBar from "../../components/NavBar";
 
-type ForgetPasswordFormProps = {
-  onSubmit: (formData: any) => void;
-};
 
-const ForgetPasswordForm = ({ onSubmit }: ForgetPasswordFormProps) => {
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
+const forgetPasswordPage = () => {
+  // const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isResetPasswordSent, setIsResetPasswordSent] = useState(false);
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+  // const [email, setEmail] = useState("");
 
-    if (!email) {
-      return toast.error(`Veuillez remplir le ${email} champ!`);
-    }
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const formData = {
-      email,
-    };
+  const handleForgetPassword = async (formData: any) => {
 
     try {
       const response = await fetch(
@@ -36,88 +31,40 @@ const ForgetPasswordForm = ({ onSubmit }: ForgetPasswordFormProps) => {
         }
       );
 
-      console.log(response);
-
       if (!response.ok) {
-        toast.error("Email n'est pas valides");
+        const data = await response.json();
+        toast.error(
+          data.message || "Une erreur s’est produite lors de la connexion"
+        );
         return;
       }
 
+      const data = await response.json();
+
       toast.success("Le jeton de réinitialisation a été envoyé avec succès !");
-      // router.push("/auto/");
+      setIsResetPasswordSent(true);
+
     } catch (error: any) {
       console.error(error);
       toast.error(
         error.message || "Une erreur s’est produite lors de la connexion"
       );
     }
-  };
 
-  const validateEmail = (value: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(value)) {
-      setEmailError("Adresse e-mail invalide");
-    } else {
-      setEmailError("");
-    }
   };
-
-  // onSubmit(formData);
-  //   };
 
   return (
     <>
       <NavBar />
-      <div
-        className="flex flex-col items-center rounded-lg border border-newColor font-default mx-auto my-28"
-        style={{ width: "650px", height: "420px" }}
-      >
-        <Image
-          src={emailIcon}
-          alt="google"
-          width={90}
-          height={90}
-          className="mt-10 flex space-x-4"
-          // style={{ width: "110px", height: "110px" }}
-        />
-        <h2 className="text-3xl font-medium text-second my-2 py-4 mb-4">
-          Mettez à jour votre mot de passe
-        </h2>
-        <form className="flex flex-col space-y-6 my-4" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-              validateEmail(e.target.value);
-            }}
-            style={{ width: "400px", margin: "auto" }}
-            className={`px-4 py-2 rounded border border-gray ${
-              emailError ? "border-red-500" : ""
-            }`}
-          />
-          {emailError && (
-            <p className="text-red-500 m-12 text-sm">{emailError}</p>
-          )}
-
-          <button
-            type="submit"
-            className="py-2 px-8 m-12 rounded-full font-medium text-base text-white bg-primary"
-          >
-            Envoyer un E-mail
-          </button>
-          <p className="font-normal my-2 mx-32 text-second">
-            Vous avez déjà un compte ?{" "}
-            <Link href="/auth/login-candidate" className="text-primary">
-              Se connecter
-            </Link>
-          </p>
-          {/* </div> */}
-        </form>
+      <div className="flex flex-col md:flex-row items-center mt-16">
+        {isResetPasswordSent ? ( 
+          <ResetPasswordSent />
+        ) : (
+          <ForgetPasswordForm onSubmit={handleForgetPassword} />
+        )}
       </div>
     </>
   );
 };
 
-export default ForgetPasswordForm;
+export default forgetPasswordPage;

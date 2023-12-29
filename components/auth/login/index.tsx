5b5883f2c5dedb98851f84b5fb7cec4d3aa2f361
Anas Zenagui <zenaguianas20@gmail.com>
2023-12-29 01:52:50 +0100

    Implementing the logic of OAuth for Google

--- components/auth/login/index.tsx
+++ components/auth/login/index.tsx
@@ -1,148 +1,192 @@
 "use client";
-import {FormEvent, useState} from "react";
-import {toast} from "react-hot-toast";
+import { FormEvent, useState } from "react";
+import { toast } from "react-hot-toast";
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 const LoginForm = (props: { loginFor: "candidate" | "enterprise" }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [emailError, setEmailError] = useState("");
-    const [passwordError, setPasswordError] = useState("");
-    const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const router = useRouter();
 
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
 
-        if (!email || !password) {
-            return toast.error("Veuillez remplir tous les champs");
+    if (!email || !password) {
+      return toast.error("Veuillez remplir tous les champs");
+    }
+
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_URL +
+          `/api/auth/${props.loginFor}/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
         }
+      );
 
-        try {
-            const response = await fetch(
-                process.env.NEXT_PUBLIC_BACKEND_URL +
-                `/api/auth/${props.loginFor}/login`,
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({email, password}),
-                }
-            );
+      console.log(response);
 
-            console.log(response);
+      if (!response.ok) {
+        toast.error("Email ou mot de passe ne sont pas valides");
+        return;
+      }
 
-            if (!response.ok) {
-                toast.error("Email ou mot de passe ne sont pas valides");
-                return;
-            }
+      console.log("Logged in successfully");
+      toast.success("connecté avec succès");
+      router.push("/dashboard");
+    } catch (error: any) {
+      console.error(error);
+      toast.error(
+        error.message || "Une erreur s’est produite lors de la connexion"
+      );
+    }
+  };
 
-            console.log("Logged in successfully");
-            toast.success("connecté avec succès");
-            router.push("/dashboard");
-        } catch (error: any) {
-            console.error(error);
-            toast.error(
-                error.message || "Une erreur s’est produite lors de la connexion"
-            );
-        }
-    };
+  const handleGoogleLogin = async () => {
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_URL + "/api/auth/google"
+      );
 
-    const validateEmail = (value: string) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(value)) {
-            setEmailError("L'adresse email invalide");
-        } else {
-            setEmailError("");
-        }
-    };
+      if (!response.ok) {
+        toast.error("Erreur lors de la connexion avec Google");
+        return;
+      }
 
-    const validatePassword = (value: string) => {
-        if (value.length < 8) {
-            setPasswordError("Le mot de passe doit comporter au moins 8 caractères");
-        } else {
-            setPasswordError("");
-        }
-    };
+      const data = await response.json();
 
-    return (
-        <div
-            className="flex flex-col items-center rounded-lg border border-newColor p-4 font-default max-w-md mx-auto md:max-w-2xl">
-            {" "}
-            <h2 className="text-2xl font-semibold text-second my-2 py-4 mb-4 text-center">
-                Inscrivez-vous pour trouver un travail que vous aimez
-            </h2>
-            <div className="mt-4 space-y-4">
-                <button
-                    onClick={() => {
-                    }}
-                    className="w-full py-2 rounded-full font-medium text-base bg-primary-3 text-white"
-                >
-                    Continuer avec Google
-                </button>
-                <button
-                    onClick={() => {
-                    }}
-                    className="w-full py-2 rounded-full font-medium text-base bg-primary-3 text-white"
-                >
-                    Continuer avec Linkedin
-                </button>
-            </div>
-            <p className="my-2 text-gray-500 font-medium text-center">Ou</p>
-            <form className="flex flex-col space-y-4 w-full" onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => {
-                        setEmail(e.target.value);
-                        validateEmail(e.target.value);
-                    }}
-                    className={`w-full px-4 py-2 rounded border border-gray ${
-                        emailError ? "border-red-500" : ""
-                    }`}
-                />
-                {emailError && <p className="text-red-500 text-sm">{emailError}</p>}
-                <input
-                    type="password"
-                    placeholder="Mot de passe (8 caractères ou plus)"
-                    value={password}
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                        validatePassword(e.target.value);
-                    }}
-                    className={`w-full px-4 py-2 rounded border border-gray ${
-                        passwordError ? "border-red-500" : ""
-                    }`}
-                />
-                {passwordError && (
-                    <p className="text-red-500 text-sm">{passwordError}</p>
-                )}
-                <p className="text-base text-second text-center">
-                    <span className="mx-1">Oublié votre</span>
-                    <Link href={`/auth/${props.loginFor}/forget-password`} className="text-primary">
-                        mot de passe?
-                    </Link>
-                </p>
-                <button
-                    type="submit"
-                    className="w-full py-2 rounded-full font-medium text-base text-white bg-primary"
-                >
-                    se connecter
-                </button>
-                <p className="my-2 text-second text-center">
-                    Je n’ai pas de compte ?{" "}
-                    <Link
-                        href={`/auth/signup-${props.loginFor}`}
-                        className="text-primary"
-                    >
-                        s'inscrire
-                    </Link>
-                </p>
-            </form>
-        </div>
-    );
+      window.location.href = data.url;
+    } catch (error: any) {
+      console.error(error);
+      toast.error("Une erreur s’est produite lors de la connexion");
+    }
+  };
+
+  const validateEmail = (value: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(value)) {
+      setEmailError("L'adresse email invalide");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const validatePassword = (value: string) => {
+    if (value.length < 8) {
+      setPasswordError("Le mot de passe doit comporter au moins 8 caractères");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center rounded-lg border border-newColor p-4 font-default max-w-md mx-auto md:max-w-2xl">
+      {" "}
+      <h2 className="text-2xl font-semibold text-second my-2 py-4 mb-4 text-center">
+        Inscrivez-vous pour trouver un travail que vous aimez
+      </h2>
+      <div className="mt-4 grid space-y-4">
+        <button
+          type="submit"
+          className="group h-12 px-20 border-2 border-gray-300 rounded-full transition duration-300 
+        hover:border-green-200 focus:bg-blue-50 active:bg-blue-100"
+          onClick={handleGoogleLogin}
+        >
+          <div className="relative flex items-center space-x-10 justify-center">
+            <img
+              src="https://tailus.io/sources/blocks/social/preview/images/google.svg"
+              className="absolute left-0 w-5"
+              alt="google logo"
+            />
+            <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-green-700 sm:text-base">
+              Continue with Google
+            </span>
+          </div>
+        </button>
+        <button
+          className="group h-12 px-20 border-2 border-gray-300 rounded-full transition duration-300 
+    hover:border-green-200 focus:bg-blue-50 active:bg-blue-100"
+        >
+          <div className="relative flex items-center space-x-10 justify-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
+              alt="LinkedIn logo"
+              className="absolute left-0 w-5"
+            />
+            <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-green-700 sm:text-base">
+              Continue with LinkedIn
+            </span>
+          </div>
+        </button>
+      </div>
+      <div className="flex items-center justify-center space-x-2 my-5">
+        <span className="h-px w-16 bg-gray-200"></span>
+        <span className="text-gray-400 font-normal">or</span>
+        <span className="h-px w-16 bg-gray-200"></span>
+      </div>
+      <form className="flex flex-col space-y-4 w-full" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            validateEmail(e.target.value);
+          }}
+          className={`w-full px-4 py-2 rounded border border-gray ${
+            emailError ? "border-red-500" : ""
+          }`}
+        />
+        {emailError && <p className="text-red-500 text-sm">{emailError}</p>}
+        <input
+          type="password"
+          placeholder="Mot de passe (8 caractères ou plus)"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            validatePassword(e.target.value);
+          }}
+          className={`w-full px-4 py-2 rounded border border-gray ${
+            passwordError ? "border-red-500" : ""
+          }`}
+        />
+        {passwordError && (
+          <p className="text-red-500 text-sm">{passwordError}</p>
+        )}
+        <p className="text-base text-second text-center">
+          <span className="mx-1">Oublié votre</span>
+          <Link
+            href={`/auth/${props.loginFor}/forget-password`}
+            className="text-primary"
+          >
+            mot de passe?
+          </Link>
+        </p>
+        <button
+          type="submit"
+          className="w-full py-2 rounded-full font-medium text-base text-white bg-primary"
+        >
+          se connecter
+        </button>
+        <p className="my-2 text-second text-center">
+          Je n’ai pas de compte ?{" "}
+          <Link
+            href={`/auth/signup-${props.loginFor}`}
+            className="text-primary"
+          >
+            s'inscrire
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
---
+++ pages/auth/login.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+
+const LoginPage = () => {
+
+  useEffect(() => {
+    const authenticateWithGoogle = async () => {
+      try {
+        // Send a request to your Laravel backend to initiate Google OAuth
+        const response = await fetch('http://localhost:8000/api/auth/google');
+        
+        // Redirect the user to the Google login page
+        const responseData = await response.json();
+        window.location.href = responseData.redirect;
+      } catch (error) {
+        console.error('Error authenticating with Google:', error);
+      }
+    };
+
+    authenticateWithGoogle();
+  }, []);
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <div>Redirecting to Google...</div>;
+    </div>
+  );
+};
+
+export default LoginPage

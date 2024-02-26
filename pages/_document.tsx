3d328998cc7d5ca6170213e29c0de78feae04d4d
sohaib <souhaibemanah@gmail.com>
2024-02-26 17:02:08 +0100

    migrate to @next/third-parties/google

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,63 +1,13 @@
 import React from "react";
-
-
-// export default function RootLayout({
-//                                        children,
-//                                    }: {
-//     children: React.ReactNode
-// }) {
-//     return (
-//         <Html lang="en">
-//         <Head>
-//
-//             <Script
-//                 async
-//                 src="https://www.googletagmanager.com/gtag/js?id=${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'}"
-//             />
-//
-//             <Script id="google-analytics">
-//                 {`
-//               window.dataLayer = window.dataLayer || [];
-//               function gtag(){dataLayer.push(arguments);}
-//               gtag('js', new Date());
-//               gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
-//           `}
-//             </Script>
-//             <title>
-//                 Facejob
-//             </title>
-//         </Head>
-//
-//         <Main>
-//         {children}
-//         </Main>
-//         </Html>
-//     )
-// }
-//
-
 import Document, {Html, Head, Main, NextScript} from 'next/document'
-import Script from "next/script";
+import {GoogleAnalytics} from '@next/third-parties/google'
 
 
 export default class CustomDocument extends Document {
     render() {
         return (
             <Html>
-
-                <Script
-                    async
-                    src="https://www.googletagmanager.com/gtag/js?id=${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'}"
-                />
-
-                <Script id="google-analytics">
-                    {`
-                              window.dataLayer = window.dataLayer || [];
-                              function gtag(){dataLayer.push(arguments);}
-                              gtag('js', new Date());
-                              gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
-                          `}
-                </Script>
+                <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}/>
                 <Head/>
                 <body>
                 <Main/>

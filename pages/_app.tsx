0ab764c0e53841d3c1cf361ec946e84783a81b37
sohaib <souhaibemanah@gmail.com>
2024-02-26 17:13:42 +0100

    fix script location

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { Toaster } from "react-hot-toast";
+import type {AppProps} from "next/app";
+import {Toaster} from "react-hot-toast";
+import {GoogleAnalytics} from "@next/third-parties/google";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <div className="font-default font-medium">
-        <Toaster position="top-center" />
-      </div>
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <div className="font-default font-medium">
+                <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!!}/>
+                <Toaster position="top-center"/>
+            </div>
+            <Component {...pageProps} />
+        </>
+    );
 }

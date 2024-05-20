d17a173d208ae6d06127c5b8fbdac974965208d9
sohaib <souhaibemanah@gmail.com>
2024-05-20 15:22:29 +0100

    update

--- app/auth/[actor]/forget-password/page.tsx
+++ app/auth/[actor]/forget-password/page.tsx
@@ -1,9 +1,10 @@
+"use client"
 import {FormEvent, useState} from "react";
 import {toast} from "react-hot-toast";
-import emailIcon from "../../../public/images/envelope.png";
+import emailIcon from "../../../../public/images/envelope.png";
 import Image from "next/image";
 import Link from "next/link";
-import NavBar from "../../../components/NavBar";
+import NavBar from "../../../../components/NavBar";
 import {useRouter} from "next/router";
 
 
--- app/auth/login-candidate/page.tsx
+++ app/auth/login-candidate/page.tsx
@@ -1,6 +1,6 @@
-import LoginForm from "../../components/auth/login";
-import NavBar from "../../components/NavBar";
-import Image from "next/image";
+"use client";
+import LoginForm from "../../../components/auth/login";
+import NavBar from "../../../components/NavBar";
 
 const LoginCandidatPage = () => {
     return (
@@ -8,15 +8,13 @@ const LoginCandidatPage = () => {
             <NavBar/>
             <div className="flex flex-col md:flex-row items-center mt-16">
                 <div className="md:w-1/2 px-4 md:px-20">
-                    <LoginForm loginFor={"enterprise"}/>
+                    <LoginForm loginFor={"candidate"}/>
                 </div>
                 <div className="w-0 px-4 md:w-1/2 mt-4 md:mt-0">
-                    <Image
+                    <img
                         src="/images/photo-login.jpg"
                         className="rounded-3xl w-full md:w-4/5 mx-auto"
                         alt="image-signup"
-                        width={1000}
-                        height={1000}
                     />
                 </div>
             </div>

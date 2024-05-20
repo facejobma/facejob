4cd2cfec0541575aae91bfb2a7747e7a29463bdc
sohaib <souhaibemanah@gmail.com>
2024-05-20 15:24:56 +0100

    fix typo

--- app/auth/login-entreprise/page.tsx
+++ app/auth/login-entreprise/page.tsx
@@ -9,7 +9,7 @@ const LoginCandidatPage = () => {
             <NavBar/>
             <div className="flex flex-col md:flex-row items-center mt-16">
                 <div className="md:w-1/2 px-4 md:px-20">
-                    <LoginForm loginFor={"enterprise"}/>
+                    <LoginForm loginFor={"entreprise"}/>
                 </div>
                 <div className="w-0 px-4 md:w-1/2 mt-4 md:mt-0">
                     <Image

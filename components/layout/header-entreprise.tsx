693ddc34825ee04d9062161889d7a4fc290f617a
Anas Zenagui <zenaguianas20@gmail.com>
2024-07-04 20:13:23 +0100

    Add an upgrad plan button at the nav header

--- components/layout/header-entreprise.tsx
+++ components/layout/header-entreprise.tsx
@@ -5,7 +5,7 @@ import { UserNav } from "./user-nav";
 import { Logo } from "@/components/ui/logo";
 import Notification from "@/components/layout/Notification";
 import { useRouter } from "next/navigation";
-import { Send, Search } from "lucide-react";
+import { Send, Star } from "lucide-react";
 
 export default function HeaderEntreprise() {
   const router = useRouter();
@@ -49,6 +49,13 @@ export default function HeaderEntreprise() {
           <MobileSidebar />
         </div>
         <div className="flex items-center gap-10">
+          <button
+            className="flex items-center gap-2 px-2 py-2 text-sm text-white font-semibold font-default rounded-md shadow-lg transition duration-300 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-600 hover:to-gray-800"
+            onClick={() => router.push("/dashboard/entreprise/payments")}
+          >
+            <Star className="w-4 h-4" />
+            Upgrade Plan
+          </button>
           <Notification />
           <UserNav />
         </div>

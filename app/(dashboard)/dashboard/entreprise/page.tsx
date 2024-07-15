cc8b638bf5197aeca8ca36494c81360962ae3440
itsmeismaill <oukhauar@gmail.com>
2024-07-15 23:42:12 +0100

    dashboard entreprise

--- app/(dashboard)/dashboard/entreprise/page.tsx
+++ app/(dashboard)/dashboard/entreprise/page.tsx
@@ -1,45 +1,65 @@
 "use client";
-import { CalendarDateRangePicker } from "@/components/date-range-picker";
-import { RecentSales } from "@/components/recent-sales";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useEffect, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
-import { Statistiques } from "@/types";
-import { Overview } from "@/components/overview";
-import * as React from "react";
-import { DateRange } from "react-day-picker";
-import { addYears } from "date-fns";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Bar } from "react-chartjs-2";
 import Cookies from "js-cookie";
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend } from "chart.js";
 
-function OverViewTab() {
-  const [stats, setStats] = useState({} as Statistiques);
-  const [date, setDate] = React.useState<DateRange | undefined>({
-    from: addYears(new Date(), -1),
-    to: new Date(),
+ChartJS.register(BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
+
+interface Stat {
+  month: number;
+  sum: number;
+}
+
+interface CandidatureByOffre {
+  titre: string;
+  sum: number;
+}
+
+interface Stats {
+  offres: Stat[];
+  candidatures: Stat[];
+  candidaturesByOffre: CandidatureByOffre[];
+  totalCandidatures: number;
+  totalOffres: number;
+}
+
+const Dashboard: React.FC = () => {
+  const [stats, setStats] = useState<Stats>({
+    offres: [],
+    candidatures: [],
+    candidaturesByOffre: [],
+    totalCandidatures: 0,
+    totalOffres: 0
   });
   const { toast } = useToast();
-
   const authToken = Cookies.get("authToken");
 
   useEffect(() => {
+    const user = JSON.parse(sessionStorage.getItem('user') || '{}');
+    const userId = user.id;
+
+    if (!userId) {
+      toast({
+        title: "Error",
+        variant: "destructive",
+        description: "User ID not found in session storage.",
+      });
+      return;
+    }
+
     async function getStats() {
-      await fetch(
-        process.env.NEXT_PUBLIC_BACKEND_URL +
-          "/api/admin/statics?from=" +
-          date?.from?.toISOString() +
-          "&to=" +
-          date?.to?.toISOString(),
-        {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-            "Content-Type": "application/json",
-          },
+      await fetch(`http://localhost:8000/api/entrepirse-stats/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          "Content-Type": "application/json",
         },
-      )
+      })
         .then((response) => response.json())
-        .then((result) => {
+        .then((result: Stats) => {
           setStats(result);
         })
         .catch((error) => {
@@ -52,198 +72,168 @@ function OverViewTab() {
     }
 
     getStats();
-  }, [date?.from, date?.to, toast]);
+  }, [toast, authToken]);
+
+  const allMonths = Array.from({ length: 12 }, (_, i) => i + 1); // [1, 2, ..., 12]
+
+  const offresData = {
+    labels: allMonths.map(m => new Date(0, m - 1).toLocaleString('fr', { month: 'short' })),
+    datasets: [{
+      label: 'Offres d’emploi diffusés',
+      data: allMonths.map(month => {
+        const found = stats.offres.find(o => o.month === month);
+        return found ? found.sum : 0;
+      }),
+      backgroundColor: 'rgb(112, 149, 93)',
+      borderColor: 'rgb(112, 149, 93)',
+      borderWidth: 1,
+      borderRadius: 3
+    }]
+  };
+
+  const candidaturesData = {
+    labels: allMonths.map(m => new Date(0, m - 1).toLocaleString('fr', { month: 'short' })),
+    datasets: [{
+      label: 'Candidatures reçues',
+      data: allMonths.map(month => {
+        const found = stats.candidatures.find(c => c.month === month);
+        return found ? found.sum : 0;
+      }),
+      backgroundColor: 'rgb(112, 149, 93)',
+      borderColor: 'rgb(112, 149, 93)',
+      borderWidth: 1,
+      borderRadius: 3
+    }]
+  };
+
+  const candidaturesByOffreData = {
+    labels: stats.candidaturesByOffre.map(c => c.titre),
+    datasets: [{
+      label: 'Candidatures reçues par offre',
+      data: stats.candidaturesByOffre.map(c => c.sum),
+      backgroundColor: 'rgb(112, 149, 93)',
+      borderColor: 'rgb(112, 149, 93)',
+      borderWidth: 1,
+      borderRadius: 3
+    }]
+  };
 
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">
-            Bonjour, bon retour 👋
-          </h2>
-          <div className="hidden md:flex items-center space-x-2">
-            <CalendarDateRangePicker date={date} setDate={setDate} />
-          </div>
+        <h2 className="text-3xl font-bold tracking-tight">Bonjour, bon retour 👋</h2>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
+          <Card>
+            <CardHeader>
+              <CardTitle>Total des offres d’emploi diffusés</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-2xl font-semibold">{stats.totalOffres}</p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Total des candidatures reçues</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-2xl font-semibold">{stats.totalCandidatures}</p>
+            </CardContent>
+          </Card>
         </div>
-        <Tabs defaultValue="overview" className="space-y-4">
-          <TabsList>
-            <TabsTrigger value="overview">Aperçu</TabsTrigger>
-            <TabsTrigger value="analytics">Plus de details</TabsTrigger>
-          </TabsList>
-          <TabsContent value="overview" className="space-y-4">
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-5">
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total des secteurs
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">
-                    {stats.sectors_count}
-                  </div>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total des postules
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">
-                    {stats.postules_count}
-                  </div>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total d&apos;offres
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <rect width="20" height="14" x="2" y="5" rx="2" />
-                    <path d="M2 10h20" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">{stats.offres_count}</div>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Nombre de candidats
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">
-                    {stats.candidates_count}
-                  </div>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Nombre d&apos;entreprises
-                  </CardTitle>
-
-                  <svg
-                    className="h-4 w-4 text-muted-foreground"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M9 7H5C3.89543 7 3 7.89543 3 9V18C3 19.1046 3.89543 20 5 20H19C20.1046 20 21 19.1046 21 18V9C21 7.89543 20.1046 7 19 7H15M9 7V5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7M9 7H15"
-                      stroke="#000000"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">
-                    {stats.entreprises_count}
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
-              <Card className="col-span-4">
-                <CardHeader>
-                  <CardTitle>Les ventes</CardTitle>
-                </CardHeader>
-                <CardContent className="pl-2">
-                  {stats.sales && <Overview unit={"DH"} stats={stats.sales} />}
-                </CardContent>
-              </Card>
-              <Card className="col-span-4 md:col-span-3">
-                <CardHeader>
-                  <CardTitle>Ventes récentes</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  {stats.last_n_sales && (
-                    <RecentSales sales={stats.last_n_sales} />
-                  )}
-                </CardContent>
-              </Card>
-            </div>
-          </TabsContent>
-          <TabsContent value="analytics" className="space-y-4">
-            <div className="grid gap-4  grid-cols-2">
-              <Card className="col-span-1">
-                <CardHeader>
-                  <CardTitle>Les enterprises</CardTitle>
-                </CardHeader>
-                <CardContent className="pl-2">
-                  {stats.entreprises && <Overview stats={stats.entreprises} />}
-                </CardContent>
-              </Card>
-              <Card className="col-span-1">
-                <CardHeader>
-                  <CardTitle>Les candidats</CardTitle>
-                </CardHeader>
-                <CardContent className="pl-2">
-                  {stats.candidates && <Overview stats={stats.candidates} />}
-                </CardContent>
-              </Card>
-            </div>
-          </TabsContent>
-        </Tabs>
+        <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-3">
+          <Card>
+            <CardHeader>
+              <CardTitle>Nombre des offres d’emploi diffusés / mois</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Bar 
+                data={offresData} 
+                options={{ 
+                  responsive: true, 
+                  plugins: { 
+                    legend: { position: 'top' }, 
+                    title: { display: false } 
+                  },
+                  scales: {
+                    y: {
+                      beginAtZero: true,
+                      ticks: {
+                        stepSize: 1,
+                        precision: 0,
+                        callback: function(value) {
+                          return Number.isInteger(value) ? value : null;
+                        }
+                      }
+                    }
+                  }
+                }} 
+              />
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Nombre des candidatures reçues / mois</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Bar 
+                data={candidaturesData} 
+                options={{ 
+                  responsive: true, 
+                  plugins: { 
+                    legend: { position: 'top' }, 
+                    title: { display: false } 
+                  },
+                  scales: {
+                    y: {
+                      beginAtZero: true,
+                      ticks: {
+                        stepSize: 1,
+                        precision: 0,
+                        callback: function(value) {
+                          return Number.isInteger(value) ? value : null;
+                        }
+                      }
+                    }
+                  }
+                }} 
+              />
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Nombre des candidatures reçues / offre d’emploi</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Bar 
+                data={candidaturesByOffreData} 
+                options={{ 
+                  responsive: true, 
+                  plugins: { 
+                    legend: { position: 'top' }, 
+                    title: { display: false } 
+                  },
+                  scales: {
+                    y: {
+                      beginAtZero: true,
+                      ticks: {
+                        stepSize: 1,
+                        precision: 0,
+                        callback: function(value) {
+                          return Number.isInteger(value) ? value : null;
+                        }
+                      }
+                    }
+                  }
+                }} 
+              />
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </ScrollArea>
   );
 }
 
-export default function page() {
-  return <OverViewTab />;
+export default function Page() {
+  return <Dashboard />;
 }

31f3c1aaa26d3534368df2a8b05d456e74d0b39b
sohaib <souhaibemanah@gmail.com>
2024-08-07 21:29:51 +0100

    remove left , right ( I don't know If I'll break the ui )

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -59,8 +58,6 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
       }}
       {...props}
     />
